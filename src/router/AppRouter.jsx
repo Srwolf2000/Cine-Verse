d6717136b4d14d8b189df4c066bcfd59d14b64fe
Stiven Restrepo <s.restrepoz2000@gmail.com>
2025-09-12 17:22:47 -0500

    fix: configure vercel.json for SPA rewrites

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router";
 import { Layout } from "../Layout/Layout";
 import HomeMedia from "../page/HomeMedia";
-import CategoryPage from '../page/CategoryPage'
+import CategoryPage from '../page/CategoryPage';
 import Search from "../page/Search";
 import DetailItems from "../components/DetailItems/DetailItems";
 import SignIn from "../page/SignIn";
@@ -11,44 +11,52 @@ import ProtectedRoute from "../components/ProtectedRoute/ProtectedRoute";
 import PublicRoute from "../components/PublicRoute/PublicRoute";
 
 function AppRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        
+      
+        <Route index element={<Navigate to="signIn" replace />} />
 
+      
+        <Route 
+          path="signUp" 
+          element={
+            <PublicRoute>
+              <SignUp />
+            </PublicRoute>
+          } 
+        />
+        <Route 
+          path="signIn" 
+          element={
+            <PublicRoute>
+              <SignIn />
+            </PublicRoute>
+          } 
+        />
 
+    
+        <Route path=":media" element={<HomeMedia />} />
+        <Route path="category/:category/:code" element={<CategoryPage />} />
+        <Route path="search" element={<Search />} />
+        <Route path=":media/view/:id" element={<DetailItems />} />
 
+  
+        <Route 
+          path="profile" 
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          } 
+        />
 
-    return (
-        <Routes>
-            <Route path="/" element={<Navigate to="/signIn" replace />} />
-
-
-
-            <Route path="/" element={<Layout />}>
-                <Route path="signUp" element={
-                    <PublicRoute>
-                        <SignUp />
-                    </PublicRoute>
-                } />
-                <Route index element={<Navigate to="/signIn" replace />} />
-                <Route path="signIn" element={
-                    <PublicRoute>
-                        <SignIn />
-                    </PublicRoute>
-                } />
-            
-                <Route path=":media" element={<HomeMedia />} />
-                <Route path="category/:category/:code" element={<CategoryPage />} />
-                <Route path="search" element={<Search />} />
-                <Route path="/:media/view/:id" element={<DetailItems />} />
-
-
-                <Route path="profile" element={
-                    <ProtectedRoute>
-                        <Profile/>
-                    </ProtectedRoute>
-                } />
-                
-            </Route>
-        </Routes>
-    )
+     
+        <Route path="*" element={<Navigate to="signIn" replace />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default AppRouter
+export default AppRouter;
